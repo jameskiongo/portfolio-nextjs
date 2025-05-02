b4b78b04c8f13e99dcddd270fae630fa9732abc9
jameskiongo <kiongojames2000@gmail.com>
2025-05-02 10:18:24 +0300

    added rajdhani font

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
 import { NavbarToggle } from "./NavbarToggle";
-import { Switch } from "@/components/ui/switch";
-import { Moon, Sun } from "lucide-react";
-import { Label } from "@/components/ui/label";
+import { Rajdhani } from "next/font/google";
+
+const rajdhani = Rajdhani({
+  weight: ["400", "500", "600", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 export default function Navbar() {
   return (
     <div>
       <div className="flex items-center justify-between p-4 ">
-        <div>Logo</div>
+        <div>
+          <h1 className={`${rajdhani.className} text-2xl font-normal`}>
+            James Kiongo
+          </h1>
+        </div>
         <div className="flex items-center space-x-4">
           <NavbarToggle />
         </div>
