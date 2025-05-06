63114448941e935a8416a0930d30cb92ae8ea4c7
jameskiongo <kiongojames2000@gmail.com>
2025-05-06 10:06:02 +0300

    removed unwanted comments

--- src/components/technologies/TechSection.jsx
+++ src/components/technologies/TechSection.jsx
@@ -10,31 +10,26 @@ export default function TechSection() {
     <div className="mt-10 px-4 sm:px-0">
       <h1 className="capitalize text-xl font-bold pb-5">Technologies</h1>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4">
-        {/* React */}
         <div className="border border-gray-300 rounded-md py-4 px-2 flex flex-col items-center gap-1 h-full">
           <ReactIcon />
           <h1 className="text-xs font-extralight">React</h1>
         </div>
 
-        {/* Django */}
         <div className="border border-gray-300 rounded-md py-4 px-2 flex flex-col items-center gap-1 h-full">
           <DjangoIcon />
           <h1 className="text-xs font-extralight">Django</h1>
         </div>
 
-        {/* SQLite */}
         <div className="border border-gray-300 rounded-md py-4 px-2 flex flex-col items-center gap-1 h-full">
           <SqlIcon />
           <h1 className="text-xs font-extralight">SQLite</h1>
         </div>
 
-        {/* Tailwind */}
         <div className="border border-gray-300 rounded-md py-4 px-2 flex flex-col items-center gap-1 h-full">
           <TailwindIcon />
           <h1 className="text-xs font-extralight">Tailwind css</h1>
         </div>
 
-        {/* Typescript */}
         <div className="border border-gray-300 rounded-md py-4 px-2 flex flex-col items-center gap-1 h-full">
           <TypescriptIcon />
           <h1 className="text-xs font-extralight">Typescript</h1>
