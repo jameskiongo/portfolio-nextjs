55db1bb2425a8f2aef1136379c98167984db5b5c
jameskiongo <kiongojames2000@gmail.com>
2025-05-06 09:33:54 +0300

    added custom favico to app

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 
@@ -15,6 +16,19 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "James Kiongo",
   description: "Created by James Kiongo",
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", rel: "shortcut icon" },
+      {
+        url: "/apple-touch-icon.png",
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+      },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
