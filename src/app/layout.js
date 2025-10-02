134f22e62da59f160aa4f75373c2e82e1f509eed
jameskiongo <kiongojames2000@gmail.com>
2025-10-02 12:26:53 +0300

    updated resume

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,51 +1,51 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Head from "next/head";
+import React from "react";
 import "./globals.css";
 import ThemeProvider from "@/components/ThemeProvider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "James Kiongo",
-  description: "Created by James Kiongo",
-  icons: {
-    icon: [
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
-      { url: "/favicon.ico", rel: "shortcut icon" },
-      {
-        url: "/apple-touch-icon.png",
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-      },
-    ],
-  },
-  manifest: "/site.webmanifest",
+	title: "James Kiongo",
+	description: "Created by James Kiongo",
+	icons: {
+		icon: [
+			{ url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+			{ url: "/favicon.svg", type: "image/svg+xml" },
+			{ url: "/favicon.ico", rel: "shortcut icon" },
+			{
+				url: "/apple-touch-icon.png",
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+			},
+		],
+	},
+	manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+			>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,37 @@
+import { Rajdhani } from "next/font/google";
 import NavbarMenu from "./NavbarMenu";
 import { NavbarToggle } from "./NavbarToggle";
-import { Rajdhani } from "next/font/google";
 
 const rajdhani = Rajdhani({
-  weight: ["400", "500", "600", "700"],
-  style: ["normal"],
-  subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	style: ["normal"],
+	subsets: ["latin"],
 });
 
 export default function Navbar() {
-  return (
-    <div>
-      <div className="flex items-center justify-between py-4">
-        <div>
-          <h1 className={`${rajdhani.className} text-2xl font-normal`}>
-            James Kiongo
-          </h1>
-        </div>
-        <div className="flex flex-row items-center justify-center">
-          <div className="flex flex-row items-center gap-10 px-2">
-            <a
-              href="/documents/james-cv-main.pdf"
-              target="_blank"
-              className="text-[20px] font-normal hover:underline"
-            >
-              CV
-            </a>
-          </div>
-          <NavbarToggle />
-          <NavbarMenu />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex items-center justify-between py-4">
+				<div>
+					<h1 className={`${rajdhani.className} text-2xl font-normal`}>
+						James Kiongo
+					</h1>
+				</div>
+				<div className="flex flex-row items-center justify-center">
+					<div className="flex flex-row items-center gap-10 px-2">
+						<a
+							href="/documents/james-cv.pdf"
+							target="_blank"
+							className="text-[20px] font-normal hover:underline"
+							rel="noopener"
+						>
+							CV
+						</a>
+					</div>
+					<NavbarToggle />
+					<NavbarMenu />
+				</div>
+			</div>
+		</div>
+	);
 }
